a3d1db01ecda9d7f247a47b554a5aa057da685d6
unknown <mtevfik41@gmail.com>
2021-05-29 03:48:50 +0300

    burclar

---
+++ Burclar/Burclar.java
@@ -0,0 +1,43 @@
+package kodluyoruzilkrepo.Burclar;
+
+import java.util.Scanner;
+
+public class Burclar {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+
+        int gun, ay;
+        System.out.print("Ayı giriniz : ");
+        ay = scn.nextInt();
+        System.out.print("Günü Giriniz : ");
+        gun = scn.nextInt();
+        scn.close();
+
+        if ((ay == 1 && gun <= 20) || (ay == 12 && gun >= 21)) {
+            System.out.println("Oğlak");
+        } else if ((ay == 1 && gun >= 21) || (ay == 2 && gun <= 19)) {
+            System.out.println("Kova");
+        } else if ((ay == 2 && gun >= 20) || (ay == 3 && gun <= 20)) {
+            System.out.println("Balık");
+        } else if ((ay == 3 && gun >= 21) || (ay == 4 && gun <= 20)) {
+            System.out.println("Koç");
+        } else if ((ay == 4 && gun >= 21) || (ay == 5 && gun <= 20)) {
+            System.out.println("Boğa");
+        } else if ((ay == 5 && gun >= 21) || (ay == 6 && gun <= 21)) {
+            System.out.println("İkizler");
+        } else if ((ay == 6 && gun >= 22) || (ay == 7 && gun <= 22)) {
+            System.out.println("Yengeç");
+        } else if ((ay == 7 && gun >= 23) || (ay == 8 && gun <= 23)) {
+            System.out.println("Aslan");
+        } else if ((ay == 8 && gun >= 24) || (ay == 9 && gun <= 23)) {
+            System.out.println("Başak");
+        } else if ((ay == 9 && gun >= 24) || (ay == 10 && gun <= 23)) {
+            System.out.println("Terazi");
+        } else if ((ay == 10 && gun >= 24) || (ay == 11 && gun <= 22)) {
+            System.out.println("Akrep");
+        } else if ((ay == 11 && gun >= 23) || (ay == 12 && gun <= 21)) {
+            System.out.println("Yay");
+        }
+
+    }
+}
