3ea060ead5dd7624a581ef8d4005da4387bd2cad
unknown <mtevfik41@gmail.com>
2021-05-29 15:34:38 +0300

    elmas odevi

--- Elmas/Elmas.java
+++ Elmas/Elmas.java
@@ -17,19 +17,6 @@ public class Elmas {
             }
             System.out.println(" ");
         }
-        // int c = 2 * n - 4 + 1;
-        // int t = 1;
-        // for (int i = 0; i < n - 1; i++) {
-        // for (int j = 0; j < t; j++) {
-        // System.out.print(" ");
-        // }
-        // for (int k = 0; k < c; k++) {
-        // System.out.print("*");
-        // }
-        // c -= 2;
-        // t += 2;
-        // System.out.println(" ");
-        // }
         for (int i = n; i >= 0; i--) {
             for (int j = 0; j < (n - i); j++) {
                 System.out.print(" ");
