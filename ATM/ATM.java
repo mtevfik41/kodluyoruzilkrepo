146d1159beb10774810f1d120d03174d8cff10bd
unknown <mtevfik41@gmail.com>
2021-05-29 15:31:22 +0300

    ATM odevi

---
+++ ATM/ATM.java
@@ -0,0 +1,54 @@
+package kodluyoruzilkrepo.ATM;
+
+import java.util.Scanner;
+
+public class ATM {
+    public static void main(String[] args) {
+        String userName, password;
+        Scanner scn = new Scanner(System.in);
+        int right = 3;
+        int balance = 1500;
+        int select;
+        while (right > 0) {
+            System.out.print("Kullanıcı Adınız :");
+            userName = scn.nextLine();
+            System.out.print("Parolanız : ");
+            password = scn.nextLine();
+
+            if (userName.equals("patika") && password.equals("dev123")) {
+                System.out.println("Merhaba, Kodluyoruz Bankasına Hoşgeldiniz!");
+                do {
+                    System.out.println("1-Para yatırma\n" + "2-Para Çekme\n" + "3-Bakiye Sorgula\n" + "4-Çıkış Yap");
+                    System.out.print("Lütfen yapmak istediğiniz işlemi seçiniz : ");
+                    select = scn.nextInt();
+                    if (select == 1) {
+                        System.out.print("Para miktarı : ");
+                        int price = scn.nextInt();
+                        balance += price;
+                    } else if (select == 2) {
+                        System.out.print("Para miktarı : ");
+                        int price = scn.nextInt();
+                        if (price > balance) {
+                            System.out.println("Bakiye yetersiz.");
+                        } else {
+                            balance -= price;
+                        }
+                    } else if (select == 3) {
+                        System.out.println("Bakiyeniz : " + balance);
+                    }
+                } while (select != 4);
+                System.out.println("Tekrar görüşmek üzere.");
+                break;
+            } else {
+                right--;
+                System.out.println("Hatalı kullanıcı adı veya şifre. Tekrar deneyiniz.");
+                if (right == 0) {
+                    System.out.println("Hesabınız bloke olmuştur lütfen banka ile iletişime geçiniz.");
+                } else {
+                    System.out.println("Kalan Hakkınız : " + right);
+                }
+            }
+        }
+        scn.close();
+    }
+}
